e68941c1f6be9c35e6d46f7545bde1d477d2ce7a
Song GAO <gaosong2006@Songs-MacBook-Pro.local>
2015-10-09 23:45:27 +0800

    clean up,style change

--- src/tassb0.f90
+++ src/tassb0.f90
@@ -5,12 +5,12 @@
 !C * coefficients of the corresponding system of equationn *
 !C *                                                       *
 !C *********************************************************
-      include './add_on/common_block.f90'
+    include './add_on/common_block.f90'
     subroutine comp_link(ielem,inode,ii) 
         use MVAR_MOD
         implicit none
-        integer :: inode,ielem
-        integer,intent(out) :: ii
+        integer,intent(out) :: ii,node,ielem
+
         integer :: i
 
         ii = 0 
@@ -21,44 +21,33 @@
         enddo
     end subroutine 
 
-    SUBROUTINE TASSB0
-          USE MVAR_MOD
-          USE PVAR_MOD
-          USE MFUNC_mod
-        USE SEBSM_MOD
-        !use elem_intgl
-        
-        IMPLICIT   NONE  
-          INTEGER  INODE,IELEM,J,JNODE,IND,INDD,IP
-          integer ::              I,II,IS,JNCON,KNCON,L
-          REAL*8  XP,YP,ZP,XSB,YSB,ZSB,R
-          REAL*8  DX,DY,DZ,NX,NY,NZ
-          REAL*8 RSN(4,4),EX(4),EY(4)
-          
-        REAL*8  BMATRIX(4,8),AMATRIX(4,8),BMAT(4)
+    subroutine tassb0
+        use mvar_mod
+        use pvar_mod
+        use mfunc_mod
+        use sebsm_mod
 
-          REAL*8  S_ANGLE
-!         REAL*8  POXY 
-          REAL*8  DSIGN
-          real(8) :: fterm_coef(0:3,4)
-        REAL*8  CELE31(4),CELE32(4),CELE33(4),AL1(4)
-          REAL*8  DPOX,DPOY,DPOZ,DPDN,PHI
+        implicit   none  
+        integer  inode,ielem,j,jnode,ind,indd,ip
+        integer ::    i,ii,is,l
+        real*8  xp,yp,zp
+        real*8  rsn(4,4)
+        real*8  bmatrix(4,8),amatrix(4,8),bmat(4)
+
+        real*8  s_angle
+        real*8  dsign
+        real(8) :: fterm_coef(0:3,4)
 
 
         DATA RSN /1.,  1.,  1.,  1., &
-     &            1., -1.,  1., -1.,&
+     &            1., -1.,  1., -1., &
      &            1.,  1., -1., -1., &
      &            1., -1., -1.,  1./ 
 !
-          DATA EX /  1.0d0,  1.0d0, -1.0d0, -1.0d0/                       
-        DATA EY /  1.0d0, -1.0d0, -1.0d0,  1.0d0/
 !
 !  ----------------------------------------------------
-!
           WRITE(10, *)   ' IN TASSB0 '
           DSDT(:)=0.0
-!         
-!  ----------------------------------------------------
 !                 
         DO 50 INODE=1, NNODE 
         L=0
@@ -91,8 +80,8 @@
 !
 50      CONTINUE
 
-        amata(:,:,:)=(0.0d0,0.0d0)
-        bmata(:,:)=(0.0d0,0.0d0)
+        amata = 0.0d0
+        bmata = 0.0d0
 
 !        open (402,file = 'xyz_matrix.txt',status = 'unknown')
 !        open (403,file = 'dxyz_matrix.txt',status = 'unknown')
@@ -126,38 +115,35 @@
         WRITE(10,*) '  INODE      ANGLE         A3   ',&
      &               '       C31           C32           C33'
 
-        do  500   inode=1,  nnf   ! source point is on the free surface
+        do  500   inode=1,  nnf
 
             xp=xyz(1,inode)
             yp=xyz(2,inode)
-            zp=xyz(3,inode) 
+            zp=xyz(3,inode)
 
             fterm_coef = 0
             call solidangle(inode,nnode,nelem,ncn,ncon,nodqua,&
              &                        h,xyz,dxyze,s_angle)    
 
             s_angle=1.0d0-s_angle
-             write(9,102)  inode, xp, yp, zp, s_angle
-             write(*,102)  inode, xp, yp, zp, s_angle
-             write(101,102)  inode, xp, yp, zp, s_angle
+            write(9,102)  inode, xp, yp, zp, s_angle
+            write(*,102)  inode, xp, yp, zp, s_angle
+            write(101,102)  inode, xp, yp, zp, s_angle
 
-        102  format(i6,3f12.4,f15.6) 
+            102  format(i6,3f12.4,f15.6) 
 
-             angle(inode)=s_angle
-
+            angle(inode)=s_angle
             amata(inode,inode,1:nsys)= angle(inode)
-!
-!  ---------------------------
-!  Integration on the free surface
+            !  ---------------------------
+            !  Integration on the free surface
 
-            do  300   ielem=1,  nelemf
+            do   ielem=1,  nelemf
 
                 write(101,*)
                 write(101,*) ' ielem=',ielem
                 write(101,*)  ' xp,yp,zp=',xp,yp,zp
 
                 call comp_link(ielem,inode,ii)
-
                 if (ii .eq. 0)   then 
                     call norm_ele1(ielem,xp,yp,zp,amatrix,bmatrix)
                 else if (ii .ne. 0)   then 
@@ -165,225 +151,119 @@
                         &                   amatrix,bmatrix)
                 end if 
                 call common_block(0,0,ielem,inode,amatrix,bmatrix,fterm_coef)
+            end do
 
-300     CONTINUE  
+            !  Integration on the body surface
 
-!  Integration on the body surface
+            do    ielem=nelemf+1,  nelem
+                write(101,*)
+                write(101,*)  ' ielem=',ielem
+                write(101,*)  ' xp,yp,zp=',xp,yp,zp
 
-        DO  400   IELEM=NELEMF+1,  NELEM
-        WRITE(101,*)
-        WRITE(101,*)  ' IELEM=',IELEM
-        WRITE(101,*)  ' XP,YP,ZP=',XP,YP,ZP
-         
-        call comp_link(ielem,inode,ii)
-!
-        if (ii .eq. 0)   then 
-            call norm_ele1(ielem,xp,yp,zp,amatrix,bmatrix)
-        else if (ii .ne. 0)   then 
-            call sing_ele1(inode,ielem,nodqua(inode),xp,yp,zp,&
-             &                   amatrix,bmatrix)
-        end if                
-!       write(101,*) ' BMATRIX=',BMATRIX(1,:)
-!       write(101,*) ' AMATRIX=',AMATRIX(1,:)
-!!
-!        do  320   j=1,  ncn(ielem) 
-!          jncon=ncon(ielem,j)
-!          kncon=ncond(ielem,j)
-!         do  320   ip=1, nsys 
-!             xsb=ex(ip)*xyz(1,jncon)
-!             ysb=ey(ip)*xyz(2,jncon)
-!             zsb=       xyz(3,jncon)
-!
-!             nx=ex(ip)*dxyz(1,kncon)
-!             ny=ey(ip)*dxyz(2,kncon)
-!             nz=       dxyz(3,kncon)
-!
-!             call dinp(xsb,ysb,zsb,dpox,dpoy,dpoz)       
-!             dpdn=dpox*nx+dpoy*ny+dpoz*nz 
-!
-!             do  is=1, nsys    
-!             if(jncon .gt. nnf)  then
-!                 amata(inode,jncon,ip)=amata(inode,jncon,ip)-&
-!         &                          rsn(is,ip)*amatrix(is,j)
-!             else
-!                 phi=poxy(xsb,ysb,zsb)
-!         bmata(inode,ip)=bmata(inode,ip)+rsn(is,ip)*amatrix(is,j)*phi   !  * ******
-!             endif
-!
-!         bmata(inode,ip)=bmata(inode,ip)-rsn(is,ip)*bmatrix(is,j)*dpdn  !  * ******
-!             enddo
-!
-!
-!        do i = 0,3
-!        call dinp0(i,xsb,ysb,zsb,phi,dpox,dpoy,dpoz)       
-!        dpdn=dpox*nx+dpoy*ny+dpoz*nz
-!        do    is=1, nsys             
-!        fterm_coef(i,ip)=fterm_coef(i,ip)-rsn(is,ip)*bmatrix(is,j)*dpdn        
-!        fterm_coef(i,ip)=fterm_coef(i,ip)+rsn(is,ip)*amatrix(is,j)*phi
-!        enddo
-!        end do 
+                call comp_link(ielem,inode,ii)
+                if (ii .eq. 0)   then 
+                    call norm_ele1(ielem,xp,yp,zp,amatrix,bmatrix)
+                else if (ii .ne. 0)   then 
+                    call sing_ele1(inode,ielem,nodqua(inode),xp,yp,zp,&
+                     &                   amatrix,bmatrix)
+                end if
+                call common_block(1,0,ielem,inode,amatrix,bmatrix,fterm_coef)
 
+            end do
 
-        call common_block(1,0,ielem,inode,amatrix,bmatrix,fterm_coef)
+            fra3(inode)=fterm_coef(0,1)!al1(1)
+            frc31(inode)=fterm_coef(1,1)!cele31(1)
+            frc32(inode)=fterm_coef(2,1)!cele32(1)
+            frc33(inode)=fterm_coef(3,1)!cele33(1)
 
-400     CONTINUE
+            !         write(10,620) inode,angle(inode),fra3(inode),&
+            !     &                 frc31(inode),frc32(inode),frc33(inode)
 
-        FrA3(INODE)=fterm_coef(0,1)!AL1(1)
-        FrC31(INODE)=fterm_coef(1,1)!CELE31(1)
-        FrC32(INODE)=fterm_coef(2,1)!CELE32(1)
-        FrC33(INODE)=fterm_coef(3,1)!CELE33(1)
-   
-         WRITE(10,620) INODE,ANGLE(INODE),FrA3(INODE),&
-     &                 FrC31(INODE),FrC32(INODE),FrC33(INODE)
-
-         phi=poxy(xp,yp,zp)
-         call dinp(xp,yp,zp,dpox,dpoy,dpoz)       
-         bmata(inode,1)=bmata(inode,1)-fra3(inode)*phi-&
-     &                 frc31(inode)*dpox-frc32(inode)*dpoy
-!        
-500     CONTINUE
+             phi=poxy(xp,yp,zp)
+             call dinp(xp,yp,zp,dpox,dpoy,dpoz)       
+             bmata(inode,1)=bmata(inode,1)-fra3(inode)*phi-&
+                 &     frc31(inode)*dpox-frc32(inode)*dpoy
+        500     CONTINUE
 !
 ! =======================================================================
 !    Source point is on the body surface
 !
-        DO  1000   INODE=NNF+1, NNODE   
-!
+        do  1000   inode=nnf+1, nnode   
 
-        XP=XYZ(1,INODE)
-        YP=XYZ(2,INODE)
-        ZP=XYZ(3,INODE) 
-!               
-        CALL SOLIDANGLE(INODE,NNODE,NELEM,NCN,NCON,NODQUA,&
-     &                    H,XYZ,DXYZE,S_ANGLE) 
+            xp=xyz(1,inode)
+            yp=xyz(2,inode)
+            zp=xyz(3,inode) 
 
-            S_ANGLE=1.0d0-S_ANGLE
+            call solidangle(inode,nnode,nelem,ncn,ncon,nodqua,&
+             &                    h,xyz,dxyze,s_angle) 
 
-            WRITE(9,102)  INODE, XP, YP, ZP, S_ANGLE
-            WRITE(*,102)  INODE, XP, YP, ZP, S_ANGLE
+            s_angle=1.0d0-s_angle
 
-            ANGLE(INODE)=S_ANGLE
+            write(9,102)  inode, xp, yp, zp, s_angle
+            write(*,102)  inode, xp, yp, zp, s_angle
 
-        DO    IP=1,  NSYS 
-            AMATA(INODE,INODE,IP)= ANGLE(INODE)
-        ENDDO
+            angle(inode)=s_angle
+
+            amata(inode,inode,1:nsys)= angle(inode)
+
 ! ------------------------------
 ! Intergration on the free surface
 ! 
-        DO  800   IELEM=1,  NELEMF
-        II=0   
-!C
-!C Using TNORWP0 to integrate 
-!C
-         CALL NORM_ELE0(IELEM,XP,YP,ZP,AMATRIX,BMATRIX)
-        call common_block(0,1,ielem,inode,amatrix,bmatrix,fterm_coef)
-!  ------
-!
-!        DO  710   J=1,  NCN(IELEM) 
-!         JNCON=NCON(IELEM,J)
-!         KNCON=NCOND(IELEM,J)
-!        DO  710   IP=1, NSYS  
-!          XSB=EX(IP)*XYZ(1,JNCON)
-!            YSB=EY(IP)*XYZ(2,JNCON)
-!            ZSB=       XYZ(3,JNCON)
-!   
-!          NX=EX(IP)*DXYZ(1,KNCON)
-!            NY=EY(IP)*DXYZ(2,KNCON)
-!            NZ=       DXYZ(3,KNCON)
-!          CALL DINP(XSB,YSB,ZSB,DPOX,DPOY,DPOZ)       
-!          DPDN=DPOX*NX+DPOY*NY+DPOZ*NZ 
-!         
-!         DO  710   IS=1, NSYS    
-!           AMATA(INODE,JNCON,IP)=AMATA(INODE,JNCON,IP)+&
-!     &                           RSN(IS,IP)*BMATRIX(IS,J)
-!             BMATA(INODE,IP)=BMATA(INODE,IP)+RSN(IS,IP)*AMATRIX(IS,J)*&
-!     &               POXY(XSB,YSB,ZSB)           
-!710     CONTINUE  
-!
+        do  800   ielem=1,  nelemf
 
-800     CONTINUE
-          
-!  --------------------------
+            ii=0   
+            call norm_ele0(ielem,xp,yp,zp,amatrix,bmatrix)
+            call common_block(0,1,ielem,inode,amatrix,bmatrix,fterm_coef)
+
+800     continue
+
 ! Intergration on the body surface    
-! 
-        DO  900   IELEM=1+NELEMF, NELEM
-        II=0   
-!C
-!C Using TSING if the source point is in the element or its mirror
-!C     elements about any symmetrical axis, otherwise using TINBOD
-!C
-        call comp_link(ielem,inode,ii)!
-        IF (II .EQ. 0)   THEN 
-         CALL NORM_ELE0(IELEM,XP,YP,ZP,AMATRIX,BMATRIX)
-        ELSE IF (II .NE. 0)   THEN 
-         CALL SING_ELE0(INODE,IELEM,NODQUA(INODE),XP,YP,ZP,&
-     &                   AMATRIX,BMATRIX)
-        END IF                
-!
 
-         DO 820 J=1,  NCN(IELEM) 
-          JNCON=NCON(IELEM,J)         
-          KNCON=NCOND(IELEM,J)
-         DO 820 IP=1, NSYS                   
-           XSB=EX(IP)*XYZ(1,JNCON)
-             YSB=EY(IP)*XYZ(2,JNCON)
-             ZSB=       XYZ(3,JNCON)
-           NX=EX(IP)*DXYZ(1,KNCON)
-             NY=EY(IP)*DXYZ(2,KNCON)
-             NZ=       DXYZ(3,KNCON)
-          CALL DINP(XSB,YSB,ZSB,DPOX,DPOY,DPOZ)       
-          DPDN=DPOX*NX+DPOY*NY+DPOZ*NZ 
-          
-           DO  IS=1, NSYS    
-             IF(JNCON .GT. NNF)  THEN
-           AMATA(INODE,JNCON,IP)=AMATA(INODE,JNCON,IP)-&
-     &                          RSN(IS,IP)*AMATRIX(IS,J)
-             ELSE       
-              PHI=POXY(XSB,YSB,ZSB)                 
-           BMATA(INODE,IP)=BMATA(INODE,IP)+RSN(IS,IP)*AMATRIX(IS,J)*PHI 
-             ENDIF
-            BMATA(INODE,IP)=BMATA(INODE,IP)-&
-     &                      RSN(IS,IP)*BMATRIX(IS,J)*DPDN       
-           ENDDO
-820     CONTINUE
-!
-900     CONTINUE
-!
-1000     CONTINUE
+        do  900   ielem=1+nelemf, nelem
+
+            call comp_link(ielem,inode,ii)!
+            if (ii .eq. 0)   then 
+                call norm_ele0(ielem,xp,yp,zp,amatrix,bmatrix)
+            else if (ii .ne. 0)   then 
+                call sing_ele0(inode,ielem,nodqua(inode),xp,yp,zp,&
+                 &                   amatrix,bmatrix)
+            end if
+
+     call common_block(1,1,ielem,inode,amatrix,bmatrix,fterm_coef)
+900     continue
+1000     continue
 !
 ! =============================================
 
-           IF( NSYS .EQ. 2) THEN
-!
-                   DO INODE=1, NNF
-                   IF(NODQUA(INODE) .EQ. 2) THEN
-                           AMATA(INODE,INODE,2)=1.0E20         
-                   ENDIF
-                   ENDDO
-!
-           ELSE IF( NSYS .EQ. 4) THEN
-!
-                   DO INODE=1, NNF
-                   IF(NODQUA(INODE) .EQ. 2) THEN
-                           AMATA(INODE,INODE,2)=1.0E20
-                           AMATA(INODE,INODE,4)=1.0E20            
-                   ELSE IF(NODQUA(INODE) .EQ. 4) THEN
-                           AMATA(INODE,INODE,3)=1.0E20
-                           AMATA(INODE,INODE,2)=1.0E20
-                   ELSE IF(NODQUA(INODE) .EQ. 5) THEN
-                           AMATA(INODE,INODE,2)=1.0E20
-                           AMATA(INODE,INODE,3)=1.0E20            
-                           AMATA(INODE,INODE,4)=1.0E20            
-                   ENDIF
-                   ENDDO
-           ENDIF
-           !-------output amata,bmata to txt file
-            do i = 1,nnode
-                 do j = 1,nnode
-                   write(400,*) amata(i,j,1:nsys)
-            end do;end do
-            do i = 1,nnode
-                    write(401,*) bmata(i,1:nsys)
-            end do
+        if( nsys .eq. 2) then
+            do inode=1, nnf
+            if(nodqua(inode) .eq. 2) then
+                amata(inode,inode,2)=1.0e20         
+            endif
+            enddo
+        else if( nsys .eq. 4) then
+            do inode=1, nnf
+            if(nodqua(inode) .eq. 2) then
+                amata(inode,inode,2)=1.0e20
+                amata(inode,inode,4)=1.0e20            
+            else if(nodqua(inode) .eq. 4) then
+                amata(inode,inode,3)=1.0e20
+                amata(inode,inode,2)=1.0e20
+            else if(nodqua(inode) .eq. 5) then
+                amata(inode,inode,2)=1.0e20
+                amata(inode,inode,3)=1.0e20            
+                amata(inode,inode,4)=1.0e20            
+            endif
+            enddo
+        endif
+        !-------output amata,bmata to txt file
+        do i = 1,nnode
+            do j = 1,nnode
+                write(400,*) amata(i,j,1:nsys)
+        end do;end do
+        do i = 1,nnode
+            write(401,*) bmata(i,1:nsys)
+        end do
 ! =============================================
 !
             DO IP=1, NSYS
