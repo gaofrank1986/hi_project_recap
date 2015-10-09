4aaec719d5112b72ce15668106e9370509b1f737
Song GAO <gaosong2006@Songs-MacBook-Pro.local>
2015-10-09 17:55:20 +0800

    remove old part for calc bmatrix

--- src/elem_proc/add_on/ElemIntgl1.f90
+++ src/elem_proc/add_on/ElemIntgl1.f90
@@ -15,10 +15,10 @@
       
         INTEGER IS,IP,N,ND,J,NP,IELEM
       
-        REAL*8  XP,YP,ZP 
-!       REAL*8  X,Y,Z,X0,Y0,Z0,XSB,YSB,ZSB       
+        real(8) ::  xp,yp,zp 
+!       real(8) ::  x,y,z,x0,y0,z0,xsb,ysb,zsb       
 
-        REAL*8  BMATRIX(4,8),AMATRIX(4,8)
+        real(8) ::  bmatrix(4,8),amatrix(4,8)
                  
           BMATRIX=0.0D0
           AMATRIX=0.0D0
@@ -156,97 +156,53 @@
 !
     subroutine sing_int1(is,ielem,nodj,xp,yp,zp,amatrix,bmatrix) 
 
-        USE MVAR_MOD
-        USE TRVar_mod    
-        USE MFUNC_mod
+        use mvar_mod
+        use trvar_mod    
+        use mfunc_mod
         use hi_intg
 
         implicit none
 
         integer,intent(in):: is,ielem,nodj
-        real*8,intent(in)::  xp,yp,zp
-        real*8,intent(out):: bmatrix(4,8),amatrix(4,8)
-        integer :: nf,ndim
+        real(8),intent(in)::  xp,yp,zp
+        real(8),intent(out):: bmatrix(4,8),amatrix(4,8)
 
-        INTEGER N,J,IP   
-        Integer Loop1,Loop2,I,NSAMB
-        INTEGER LI,LJ,LK,INODE,INODD
-!
+        integer ::inode,inodd,j,pwr_g
 
-        real(8) :: src_lcl(2),src_glb(3)
+        real(8) :: src_lcl(2),src_glb(3),origin_offset(3)
         real(8) :: cnr_glb_mtx(3,8)
-        real(8) ::  sf_src(8),dsf_src(2,8),ddsf_src(3,8)
-        !shape function ,derivative, double derivatives based on local src point
-        real(8) ::  SF_iter(8),DSF_iter(2,8),DDSF_iter(3,8),DET1,DET2,DET3,DET 
-        !shape function,..... based on iteration point
+
+        real(8) :: result0(8),result1(8)
+        real(8) ::  x0,y0,z0,si,eta
+
+        real(8) ::  xiqsi(8),xiqet(8)
+        real(8) ::  ex(4),ey(4)
 
 
-
-        REAL*8  EX(4,4),EY(4,4)
-        REAL*8  X,Y,Z,X0,Y0,Z0,DX,DY,DZ,R,GXF(4)!,GXF0(4),GXF1(4)       
-        REAL*8  XSB,YSB,ZSB,DUM,WKX,PHI  
-        REAL*8  Nx,Ny,Nz,DAREA
-        REAL*8  DPOX,DPOY,DPOZ,DGN,DGN0,DGN1
-        REAL*8  XIQSI(8),XIQET(8),XITSI(6),XITET(6),SI,ETA,DUMX
-        real*8  Xiq(8),Wiq(8)!,Xit(7),EtaT(7),WIT(7)
-        REAL*8  SISM,ETASM
-        REAL*8  XJ(2,3),XJSM(2,3),XXJ(3,3),XJp(2,3)
-
-        REAL*8  GRN0J(8),GRN1J(8)
-
-        integer :: debug_flag = 1,pwr_g
-        real(8) :: result0(8),result1(8),ctr_glb(3)
-
-        real(8) :: XIT(7),ETAT(7),WIT(9)
-        real(8) :: PLO,CSST,SNST,SF,DSF,N0,N1C,N1S,JK0
-
-      DATA EX/  1.0d0,  1.0d0, -1.0d0, -1.0d0,    &
-                1.0d0,  1.0d0, -1.0d0, -1.0d0,    &
-               -1.0d0, -1.0d0,  1.0d0,  1.0d0,    &
-               -1.0d0, -1.0d0,  1.0d0,  1.0d0/
-
-      DATA EY/  1.0d0, -1.0d0, -1.0d0,  1.0d0,    &
-               -1.0d0,  1.0d0,  1.0d0, -1.0d0,    &
-               -1.0d0,  1.0d0,  1.0d0, -1.0d0,    &
-                1.0d0, -1.0d0, -1.0d0,  1.0d0/
+        DATA EX/  1.0d0,  1.0d0, -1.0d0, -1.0d0/                                                  
+        DATA EY/  1.0d0, -1.0d0, -1.0d0,  1.0d0/
 
         ! ** XIQSI AND XIQET: NODE COORDINATES FOR QUADRILATERAL ELEMENTS
-        DATA XIQSI/-1.0d0, 0.0d0, 1.0d0, 1.0d0, 1.0d0, 0.0d0,-1.0d0,-1.0d0/
-        DATA XIQET/-1.0d0,-1.0d0,-1.0d0, 0.0d0, 1.0d0, 1.0d0, 1.0d0, 0.0d0/    
+        data xiqsi/-1.0d0, 0.0d0, 1.0d0, 1.0d0, 1.0d0, 0.0d0,-1.0d0,-1.0d0/
+        data xiqet/-1.0d0,-1.0d0,-1.0d0, 0.0d0, 1.0d0, 1.0d0, 1.0d0, 0.0d0/    
 
-    DATA XIQ/ 0.960289856497536D+00, 0.796666477413626D+00, &
-              0.525532409916329D+00, 0.183434642495650D+00, &
-             -0.183434642495650D+00,-0.525532409916329D+00, &
-             -0.796666477413626D+00,-0.960289856497536D+00/
-  
-    DATA WIQ/ 0.101228536290376D+00, 0.222381034453374D+00, &
-              0.313706645877887D+00, 0.362683783378362D+00, &
-              0.362683783378362D+00, 0.313706645877887D+00, &
-              0.222381034453374D+00, 0.101228536290376D+00/     
-!    ============================================    
 
         inode=ncon(ielem,nodj) ! corresponding node id
         inodd=ncond(ielem,nodj)!                 normal id
-
-        write(110,*) '   ELEMT ID =',ielem
-        write(110,*) '   NODE ID =',INODE
 
         if(ncn(ielem).eq.8)  then 
 
             si =xiqsi(nodj) !get local coordinate for the src
             eta=xiqet(nodj)
-            call spfunc8_1(si,eta,sf_src,dsf_src,ddsf_src) 
-            ! sf_src,dsf_src,ddsf_src is based on si,eta which is one node of the element
 
         else if(ncn(ielem).eq.6)  then
-!           si =xitsi(nodj)
-!           eta=xitet(nodj)
-!           call spfunc6_1(si,eta,sf_src,dsf_src,ddsf_src) 
+            !si =xitsi(nodj)
+            !eta=xitet(nodj)
         endif
 
         ! get src point in sysmetric mesh
-        x0=ex(is,1)*xp
-        y0=ey(is,1)*yp      
+        x0=ex(is)*xp
+        y0=ey(is)*yp      
         z0=zp
 
         ! if mesh is created by using symmetrical information
@@ -258,15 +214,9 @@
         ! they has to match each other, the relationship cannot be reflected here
         ! since both NODJ,and XP-ZP are input information
 
-        src_glb(1) = xp
-        src_glb(2) = yp
-        src_glb(3) = zp
-        src_lcl(1) = si
-        src_lcl(2) = eta
-
-        ctr_glb(1) = 0
-        ctr_glb(2) = 0
-        ctr_glb(3) = 0
+        src_glb(:) = (/xp,yp,zp/)
+        src_lcl(:) = (/si,eta/)
+        origin_offset(:) = (/0,0,0/)
 
         !========switch cnr_glb_mtx order for hi module
         cnr_glb_mtx(:,1) = xyz(:,ncon(ielem,1))
@@ -278,89 +228,15 @@
         cnr_glb_mtx(:,7) = xyz(:,ncon(ielem,6))
         cnr_glb_mtx(:,8) = xyz(:,ncon(ielem,8))
 
-        pwr_g = ncn(ielem)/2+(ncn(ielem)/9)*2 
-        call set_npwg(pwr_g)
-        call set_src_preset(si,eta,xyz(1:3,ncon(ielem,nodj)),ctr_glb)
 
-        nf=8;ndim=3
-
+        call set_src_preset(si,eta,xyz(1:3,ncon(ielem,nodj)),origin_offset)
         call eval_singular_elem(cnr_glb_mtx,result0,result1)
+
         do j=1, ncn(ielem)
             amatrix(is,j) = result0(j)
             bmatrix(is,j) = result1(j)
         end do
-        write(110,*) '   sieppem result',result0
-        write(110,*) 'sum of amatrix',sum(amatrix(is,1:8))
 
-        
-        
-        
-        NSAMB=0
-
-
-        if(ncn(ielem).eq.8)  then 
-
-            do 500 loop1=1, 8 
-            do 500 loop2=1, 8      
-
-            nsamb=nsamb+1     
-
-            sism=xiq(loop1)
-            etasm=xiq(loop2)  
-
-            call spfunc8_1(sism,etasm,sf_iter,dsf_iter,ddsf_iter)
-
-            X=0.0D0
-            Y=0.0D0
-            Z=0.0D0
-            NX=0.0D0
-            NY=0.0D0
-            NZ=0.0D0
-
-            DO  LK=1,  NCN(IELEM)! 8 
-                X  = X  + SF_iter(LK)*XYZ(1,NCON(IELEM,LK))  ! change ksi,eta to glb position  
-                Y  = Y  + SF_iter(LK)*XYZ(2,NCON(IELEM,LK))
-                Z  = Z  + SF_iter(LK)*XYZ(3,NCON(IELEM,LK))
-                NX = NX + SF_iter(LK)*DXYZ(1,NCOND(IELEM,LK)) !get normal at eta,ksi  
-                NY = NY + SF_iter(LK)*DXYZ(2,NCOND(IELEM,LK))
-                NZ = NZ + SF_iter(LK)*DXYZ(3,NCOND(IELEM,LK))
-            END DO
-
-            do  li=1,2
-                do lj=1,3
-                    dumx=0.0d0
-                    do    lk=1,ncn(ielem)
-                        dumx=dumx+dsf_iter(li,lk)*xyz(lj,ncon(ielem,lk))
-                    enddo
-                    xjp(li,lj)=dumx
-                end do ! lj
-            end do !Li
-
-            det1=xjp(1,2)*xjp(2,3)-xjp(1,3)*xjp(2,2)    !  j1
-            det2=xjp(1,3)*xjp(2,1)-xjp(1,1)*xjp(2,3)    !  j2
-            det3=xjp(1,1)*xjp(2,2)-xjp(1,2)*xjp(2,1)    !  j3
-            det=dsqrt(det1*det1+det2*det2+det3*det3)
-            darea=det*wiq(loop1)*wiq(loop2) !only for j
-
-            call dtgrn (h,x,x0,y,y0,z,z0,gxf) 
-            dgn=gxf(2)*nx+gxf(3)*ny+gxf(4)*nz
-            ! DGN is derivative of Green fucntion over normal vector,
-            ! partical G over partial n
-
-            do j=1, ncn(ielem)
-               ! bmatrix(is,j)=bmatrix(is,j)+gxf(1)*darea*sf_iter(j)
-            enddo
-
-            500    continue
-
-        write (501,*) "bmatrix"
-        write (501,1000) bmatrix(is,1:8)
-        write (501,*) "eval result" 
-        write (501,1000) result1
-        1000 format(8f10.6)
-        else if(ncn(ielem).eq.6)  then
-
-        endif
 
     end subroutine 
                
