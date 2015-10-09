330a148771cfe0c186548462a171b0a3909fdac0
Song GAO <gaosong2006@Songs-MacBook-Pro.local>
2015-10-09 10:32:28 +0800

    clean up comment line,fix indentation and style

--- src/elem_proc/add_on/ElemIntgl1.f90
+++ src/elem_proc/add_on/ElemIntgl1.f90
@@ -23,10 +23,10 @@
           BMATRIX=0.0D0
           AMATRIX=0.0D0
 
-        do IS=1,   NSYS  
-          CALL NORM_INT1(IS,IELEM,NCN(IELEM),XP,YP,ZP,AMATRIX,BMATRIX) 
-          ! ncn(?) is element type
-        end do
+          do IS=1,   NSYS  
+              CALL NORM_INT1(IS,IELEM,NCN(IELEM),XP,YP,ZP,AMATRIX,BMATRIX) 
+              ! ncn(?) is element type
+          end do
 !
         END           
 
@@ -57,24 +57,24 @@
           AMATRIX= 0.0d0  
 !
         IF(NUMQUA.EQ.0)       THEN
-         do IS=1,  NSYS
-          IF(IS.EQ.1) THEN 
-            CALL SING_INT1(IS,IELEM,NODNUM,XP,YP,ZP,AMATRIX,BMATRIX)
-          ELSE IF( IS.NE.1 ) THEN   
-            CALL NORM_INT1(IS,IELEM,NCN(IELEM),XP,YP,ZP,AMATRIX,BMATRIX)
-           ! write(*,*) 'After Subroutine SGWP0_1'
-          END IF
-         end do
+            do IS=1,  NSYS
+                IF(IS.EQ.1) THEN 
+                    CALL SING_INT1(IS,IELEM,NODNUM,XP,YP,ZP,AMATRIX,BMATRIX)
+                ELSE IF( IS.NE.1 ) THEN   
+                    CALL NORM_INT1(IS,IELEM,NCN(IELEM),XP,YP,ZP,AMATRIX,BMATRIX)
+                    ! write(*,*) 'After Subroutine SGWP0_1'
+                END IF
+             end do
 !
         ELSE IF(NUMQUA.EQ.2) THEN
-         DO 200 IS=1,NSYS     
-          IF(IS.EQ.1.OR.IS.EQ.2) THEN  
-            CALL SING_INT1(IS,IELEM,NODNUM,XP,YP,ZP,AMATRIX,BMATRIX)
-          ELSE IF(IS.EQ.3.OR.IS.EQ.4) THEN  
-            CALL NORM_INT1(IS,IELEM,NCN(IELEM),XP,YP,ZP,AMATRIX,BMATRIX) 
-          END IF
-!
-200      CONTINUE  
+        DO 200 IS=1,NSYS     
+            IF(IS.EQ.1.OR.IS.EQ.2) THEN  
+                CALL SING_INT1(IS,IELEM,NODNUM,XP,YP,ZP,AMATRIX,BMATRIX)
+            ELSE IF(IS.EQ.3.OR.IS.EQ.4) THEN  
+                CALL NORM_INT1(IS,IELEM,NCN(IELEM),XP,YP,ZP,AMATRIX,BMATRIX) 
+            END IF
+            !
+        200      CONTINUE  
 !
         ELSE IF(NUMQUA.EQ.4) THEN
          DO 300  IS=1,  NSYS
@@ -154,8 +154,8 @@
 ! *                                                           *
 ! *************************************************************
 !
-        SUBROUTINE SING_INT1(IS,IELEM,NODJ,XP,YP,ZP,AMATRIX,BMATRIX) 
-! 
+    subroutine sing_int1(is,ielem,nodj,xp,yp,zp,amatrix,bmatrix) 
+
         USE MVAR_MOD
         USE TRVar_mod    
         USE MFUNC_mod
@@ -175,7 +175,7 @@
 
         real(8) :: src_lcl(2),src_glb(3)
         real(8) :: cnr_glb_mtx(3,8)
-        real(8) ::  SF_src(8),DSF_src(2,8),DDSF_src(3,8)
+        real(8) ::  sf_src(8),dsf_src(2,8),ddsf_src(3,8)
         !shape function ,derivative, double derivatives based on local src point
         real(8) ::  SF_iter(8),DSF_iter(2,8),DDSF_iter(3,8),DET1,DET2,DET3,DET 
         !shape function,..... based on iteration point
@@ -195,12 +195,10 @@
         REAL*8  GRN0J(8),GRN1J(8)
 
         integer :: debug_flag = 1,pwr_g
-        integer :: debug_file_id = 108
         real(8) :: result0(8),ctr_glb(3)
 
         real(8) :: XIT(7),ETAT(7),WIT(9)
         real(8) :: PLO,CSST,SNST,SF,DSF,N0,N1C,N1S,JK0
-        real(8) :: JK1C,JK1S,F1,F2,F,TOT,TOTJ,GXF0,GXF1
 
       DATA EX/  1.0d0,  1.0d0, -1.0d0, -1.0d0,    &
                 1.0d0,  1.0d0, -1.0d0, -1.0d0,    &
@@ -226,9 +224,9 @@
               0.362683783378362D+00, 0.313706645877887D+00, &
               0.222381034453374D+00, 0.101228536290376D+00/     
 !    ============================================    
-      inode=ncon(ielem,nodj) ! corresponding node id
-      inodd=ncond(ielem,nodj)!                 normal id
 
+        inode=ncon(ielem,nodj) ! corresponding node id
+        inodd=ncond(ielem,nodj)!                 normal id
 
         write(110,*) '   ELEMT ID =',ielem
         write(110,*) '   NODE ID =',INODE
@@ -237,7 +235,6 @@
 
             si =xiqsi(nodj) !get local coordinate for the src
             eta=xiqet(nodj)
-
             call spfunc8_1(si,eta,sf_src,dsf_src,ddsf_src) 
             ! sf_src,dsf_src,ddsf_src is based on si,eta which is one node of the element
 
@@ -247,34 +244,30 @@
 !           call spfunc6_1(si,eta,sf_src,dsf_src,ddsf_src) 
         endif
 
-         
-        write(debug_file_id,*) '   SI=',SI,'   ETA=',ETA
+        ! get src point in sysmetric mesh
+        x0=ex(is,1)*xp
+        y0=ey(is,1)*yp      
+        z0=zp
 
-            X0=EX(IS,1)*XP
-            Y0=EY(IS,1)*YP      
-            Z0=ZP
+        ! if mesh is created by using symmetrical information
+        ! basically, we get the same local layout
+        ! however, the glb position of the src point is different
+        ! this will lead to ...
+        ! when the src information is passed to the integration 
+        ! both src local pos and global pos is set
+        ! they has to match each other, the relationship cannot be reflected here
+        ! since both NODJ,and XP-ZP are input information
 
-            ! if mesh is created by using symmetrical information
-            ! basically, we get the same local layout
-            ! however, the glb position of the src point is different
-            ! this will lead to ...
-            ! when the src information is passed to the integration 
-            ! both src local pos and global pos is set
-            ! they has to match each other, the relationship cannot be reflected here
-            ! since both NODJ,and XP-ZP are input information
+        src_glb(1) = xp
+        src_glb(2) = yp
+        src_glb(3) = zp
+        src_lcl(1) = si
+        src_lcl(2) = eta
 
-            src_glb(1) = xp
-            src_glb(2) = yp
-            src_glb(3) = zp
-            src_lcl(1) = si
-            src_lcl(2) = eta
+        ctr_glb(1) = 0
+        ctr_glb(2) = 0
+        ctr_glb(3) = 0
 
-            ctr_glb(1) = 0
-            ctr_glb(2) = 0
-            ctr_glb(3) = 0
-        write(110, *)  '  src_ctr_glb    ctr_glb=',ctr_glb
-
-        pwr_g = ncn(ielem)/2+(ncn(ielem)/9)*2 
         !========switch cnr_glb_mtx order for hi module
         cnr_glb_mtx(:,1) = xyz(:,ncon(ielem,1))
         cnr_glb_mtx(:,2) = xyz(:,ncon(ielem,3))
@@ -285,6 +278,7 @@
         cnr_glb_mtx(:,7) = xyz(:,ncon(ielem,6))
         cnr_glb_mtx(:,8) = xyz(:,ncon(ielem,8))
 
+        pwr_g = ncn(ielem)/2+(ncn(ielem)/9)*2 
         call set_npwg(pwr_g)
         call set_src_preset(si,eta,xyz(1:3,ncon(ielem,nodj)),ctr_glb)
 
@@ -292,32 +286,30 @@
 
         call eval_singular_elem(cnr_glb_mtx,result0)
 
-        write (12,*) ielem,NODj,is,result0
+        do j=1, ncn(ielem)
+            amatrix(is,j) = result0(j)
 
+        end do
+        write(110,*) '   sieppem result',result0
+        write(110,*) 'sum of amatrix',sum(amatrix(is,1:8))
 
+        
+        
+        
         NSAMB=0
-!
-! **** FOR QUADRILATERIAL ELEMENTS  ****************
-!
-       write(debug_file_id,*) ' Surface Integration'
 
-        GRN0J(:)=0.0d0  
-        GRN1J(:)=0.0d0
 
-    IF(NCN(IELEM).EQ.8)  THEN 
+        if(ncn(ielem).eq.8)  then 
 
-        DO 500 LOOP1=1, 8 
-            DO 500 LOOP2=1, 8      
-                NSAMB=NSAMB+1     
-!         write(debug_file_id,*)  ' NSAMB=',NSAMB                    
+            do 500 loop1=1, 8 
+            do 500 loop2=1, 8      
 
-            SISM=XIQ(loop1)
-            ETASM=XIQ(loop2)  
-!             PLO=DSQRT((SISM-SI)*(SISM-SI)+(ETASM-ETA)*(ETASM-ETA))
-!             CSST=(SISM-SI)/PLO 
-!             SNST=(ETASM-ETA)/PLO 
- 
-            CALL SPFUNC8_1(SISM,ETASM,SF_iter,DSF_iter,DDSF_iter)
+            nsamb=nsamb+1     
+
+            sism=xiq(loop1)
+            etasm=xiq(loop2)  
+
+            call spfunc8_1(sism,etasm,sf_iter,dsf_iter,ddsf_iter)
 
             X=0.0D0
             Y=0.0D0
@@ -334,163 +326,37 @@
                 NY = NY + SF_iter(LK)*DXYZ(2,NCOND(IELEM,LK))
                 NZ = NZ + SF_iter(LK)*DXYZ(3,NCOND(IELEM,LK))
             END DO
-           
-            DO  LI=1,2
-                DO LJ=1,3
-                    DUMX=0.0D0
-                    DO    LK=1,NCN(IELEM)
-                        DUMX=DUMX+DSF_iter(LI,LK)*XYZ(LJ,NCON(IELEM,LK))
-                    Enddo
-                    XJP(LI,LJ)=DUMX
-                end do ! Lj
+
+            do  li=1,2
+                do lj=1,3
+                    dumx=0.0d0
+                    do    lk=1,ncn(ielem)
+                        dumx=dumx+dsf_iter(li,lk)*xyz(lj,ncon(ielem,lk))
+                    enddo
+                    xjp(li,lj)=dumx
+                end do ! lj
             end do !Li
 
-            DET1=XJP(1,2)*XJP(2,3)-XJP(1,3)*XJP(2,2)    !  J1
-            DET2=XJP(1,3)*XJP(2,1)-XJP(1,1)*XJP(2,3)    !  J2
-            DET3=XJP(1,1)*XJP(2,2)-XJP(1,2)*XJP(2,1)    !  J3
-            DET=DSQRT(DET1*DET1+DET2*DET2+DET3*DET3)
-        !
-            DAREA=DET*WIQ(LOOP1)*WIQ(LOOP2) !only for J
+            det1=xjp(1,2)*xjp(2,3)-xjp(1,3)*xjp(2,2)    !  j1
+            det2=xjp(1,3)*xjp(2,1)-xjp(1,1)*xjp(2,3)    !  j2
+            det3=xjp(1,1)*xjp(2,2)-xjp(1,2)*xjp(2,1)    !  j3
+            det=dsqrt(det1*det1+det2*det2+det3*det3)
+            darea=det*wiq(loop1)*wiq(loop2) !only for j
 
-!             write(debug_file_id,*)  ' DAREA=',DAREA                    
+            call dtgrn (h,x,x0,y,y0,z,z0,gxf) 
+            dgn=gxf(2)*nx+gxf(3)*ny+gxf(4)*nz
+            ! DGN is derivative of Green fucntion over normal vector,
+            ! partical G over partial n
 
-!  ---------------------------------------------
+            do j=1, ncn(ielem)
+                bmatrix(is,j)=bmatrix(is,j)+gxf(1)*darea*sf_iter(j)
+            enddo
 
+            500    continue
 
-          
-        ! x0-z0, src_ctr_glb for green fucntion
+        else if(ncn(ielem).eq.6)  then
 
+        endif
 
-!           CALL DTGRN0(H,X,X0,Y,Y0,Z,Z0,GXF0)
-!         DGN0=GXF0(2)*NX+GXF0(3)*NY+GXF0(4)*NZ
-!          write(debug_file_id,*)  ' GXF0=',GXF0(2),GXF0(3),GXF0(4)                    
-!          write(debug_file_id,*)  ' DGN0=',DGN0                    
-              
-! !        
-!           CALL DTGRN1(H,X,X0,Y,Y0,Z,Z0,GXF1) 
-!         DGN1=GXF1(2)*NX+GXF1(3)*NY+GXF1(4)*NZ
-! !         write(debug_file_id,*)  ' GXF1=',GXF1                    
-! !         write(debug_file_id,*)  ' DGN1=',DGN1                    
-
-        CALL DTGRN (H,X,X0,Y,Y0,Z,Z0,GXF) 
-        DGN=GXF(2)*NX+GXF(3)*NY+GXF(4)*NZ
-        ! DGN is derivative of Green fucntion over normal vector,
-        ! partical G over partial n
-
-        DO J=1, NCN(IELEM)
-!             AMATRIX(IS,J)=AMATRIX(IS,J)+DGN*DAREA*SF_iter(J)
-            ! difference only exists for using SF_iter(J) and Darea,whch is equivalent ot Jacobian matrix
-
-!             GRN0J(J)=GRN0J(J)+DGN0*DAREA*SF_iter(J)
-!             GRN1J(J)=GRN1J(J)+DGN1*DAREA*SF_iter(J) 
-
-            BMATRIX(IS,J)=BMATRIX(IS,J)+GXF(1)*DAREA*SF_iter(J)
-            ! B matrix is only G over sufrace
-
-        ENDDO
-     
-500    CONTINUE
-!
-
-!
-! **** FOR TRIANGULAR ELEMENTS **********************
-! 
-        ELSE IF(NCN(IELEM).EQ.6)  THEN
-!
-!       DO 800 NSAMB=1, 7
-      
-!        SISM=XIT(NSAMB)
-!        ETASM=ETAT(NSAMB) 
-!        PLO=DSQRT((SISM-SI)*(SISM-SI)+(ETASM-ETA)*(ETASM-ETA))
-!        CSST=(SISM-SI)/PLO 
-!        SNST=(ETASM-ETA)/PLO 
-      
-!        CALL SPFUNC6_1(SISM,ETASM,SF,DSF,DDSF)
-
-!       X=0.0D0
-!       Y=0.0D0
-!       Z=0.0D0
-!       NX=0.0D0
-!       NY=0.0D0
-!       NZ=0.0D0
-!       DO  LK=1,  NCN(IELEM)
-!         X=X+SF(LK)*XYZ(1,NCON(IELEM,LK))    
-!         Y=Y+SF(LK)*XYZ(2,NCON(IELEM,LK))
-!         Z=Z+SF(LK)*XYZ(3,NCON(IELEM,LK))
-!         NX=NX+SF(LK)*DXYZ(1,NCOND(IELEM,LK))    
-!         NY=NY+SF(LK)*DXYZ(2,NCOND(IELEM,LK))
-!         NZ=NZ+SF(LK)*DXYZ(3,NCOND(IELEM,LK))
-!       END DO
-         
-!           DO  630  LI=1,2
-!           DO  630  LJ=1,3
-!           DUMX=0.0D0
-!           DO    LK=1,NCN(IELEM)
-!          DUMX=DUMX+DSF(LI,LK)*XYZ(LJ,NCON(IELEM,LK))
-!         enddo
-!            XJP(LI,LJ)=DUMX
-! 630      CONTINUE
-! !
-!           DET1=XJP(1,2)*XJP(2,3)-XJP(1,3)*XJP(2,2)    !  J1
-!           DET2=XJP(1,3)*XJP(2,1)-XJP(1,1)*XJP(2,3)    !  J2
-!           DET3=XJP(1,1)*XJP(2,2)-XJP(1,2)*XJP(2,1)    !  J3
-!           DET=DSQRT(DET1*DET1+DET2*DET2+DET3*DET3)
-
-!         DAREA=DET*WIT(NSAMB)
-!  !  ---------------------------------------------
-       
-!         DO J=1, NCN(IELEM)
-!         N0 =SF_src(J)
-!         N1C=DSF_src(1,J)
-!         N1S=DSF_src(2,J)   
-       
-!        CALL AREA_COEF(CSST,SNST,JK0,JK1C,JK1S,   &
-!                        N0,N1C,N1S,XJ,XXJ,F1,F2)
-
-!         F=F2/PLO/PLO+F1/PLO                       ! (C19)
-!         TOT=F/PLO*WIT(NSAMB)
- 
-!         AMATRIX(IS,J)=AMATRIX(IS,J)-TOT
-!         TOTJ(J)=TOTJ(J)-TOT
-!        ENDDO
-
-! !  ---------------------------------------------
-
-!         X0=EX(IS,1)*XP
-!         Y0=EY(IS,1)*YP        
-!         Z0=ZP        
-!           CALL DTGRN0(H,X,X0,Y,Y0,Z,Z0,GXF0)       
-!         DGN0=GXF0(2)*NX+GXF0(3)*NY+GXF0(4)*NZ
-     
-!           CALL DTGRN1(H,X,X0,Y,Y0,Z,Z0,GXF1) 
-!         DGN1=GXF1(2)*NX+GXF1(3)*NY+GXF1(4)*NZ        
-
-!           CALL DTGRN (H,X,X0,Y,Y0,Z,Z0,GXF) 
-!          DGN=GXF(2)*NX+GXF(3)*NY+GXF(4)*NZ
-        
-!         DO J=1, NCN(IELEM)
-!          AMATRIX(IS,J)=AMATRIX(IS,J)+DGN*DAREA*SF(J)
-! !        GRN0J(J)=GRN0J(J)+DGN0*DAREA*SF(J)
-! !        GRN1J(J)=GRN1J(J)+DGN1*DAREA*SF(J)
-!          BMATRIX(IS,J)=BMATRIX(IS,J)+GXF(1)*DAREA*SF(J)
-!        ENDDO
-
-700    CONTINUE     
-
-800    CONTINUE 
-
-        ENDIF
-!
-! ======================================================================
-!
-
-
-
-!
-        DO J=1, NCN(IELEM)
-            AMATRIX(IS,J) = result0(J)
-        END DO
-        write(110,*) '   sieppem result',result0
-        write(110,*) 'sum of AMATRIX',SUM(AMATRIX(IS,1:8))
-        END
+    end subroutine 
                
