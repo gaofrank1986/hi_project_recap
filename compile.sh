e822bc69705054eec5bd2ed90b5fb53907f015c9
Song GAO <gaosong2006@Songs-MacBook-Pro.local>
2015-10-11 12:46:44 +0800

    create data folder,reorganised files

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-ifort ./src/*.f ./src/*.f90 ./src/elem_proc/ElemIntgl0.f90 ./src/hi_intgl/*.f90 -I./module/
+ifort ./src/*.f ./src/*.f90 ./src/data/*.f90 ./src/elem_proc/ElemIntgl0.f90 ./src/hi_intgl/*.f90 -I./module/
 mv *.mod ./module/
