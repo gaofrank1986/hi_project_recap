5825c52983a5ad2b9397fde51bf325064a351ec2
song gao <gaosong2006@gmail.com>
2016-03-29 16:12:12 +0800

    changed script file

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
-ifort ./src/*.f ./src/*.f90 ./src/data/*.f90  ./src/hi_intgl/*.f90 ./src/property/*.f90 -I./module/ 
-mv *.mod ./module/
+#ifort ./src/*.f ./src/*.f90 ./src/data/*.f90  ./src/hi_intgl/*.f90 ./src/property/*.f90 -I./build/module/ 
+#mv *.mod ./build/module/
+cd build
+make
+cd ..
