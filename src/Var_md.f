4afd3404693a6b3f8e08575f476ecd1011322c7e
song gao <gaosong2006@gmail.com>
2016-04-07 13:57:33 +0800

    replaced pvar_mod by motion,add fortran debug flag

--- src/Var_md.f
+++ src/Var_md.f
@@ -222,32 +222,32 @@ C
 !C Variable for potential, force and body motion
 !C =============================================
 !C
-        MODULE PVar_mod
-C
-          INTEGER  NNT
-          PARAMETER (NNT=2000)    
-
-        REAL*8 XC,YC,ZC,XTC,YTC,ZTC
-        REAL*8 ARE,XF,YF,XK2,YK2,XCF
-        REAL*8 VOLM,XB,YB,ZB
-
-        REAL*8 AMAS(6,6),BDMP(6,6)  
-        REAL*8 RMAS(6,6),CDMP(6,6),CRS(6,6),STKM(6,6),XIA(3,3)
-C
-        REAL*8 FORCEW(6),FORCE0(6),FORSCD(6),AMPJ(6)
-          REAL*8 FORCE(6),FORCE_O(6)
-C
-        REAL*8 TRMAS(6,6),VISC(6,6)
-
-        REAL*8  DISP(6),DSDT(6),DISP_O(6)
-        real(8) ::  DSDT_O(6),DSDDTL(6),TRXYZ(3,3)   
-
-        REAL*8  RESPR(6),RESPI(6)  
-!
-!
-        END MODULE PVar_mod
-
+        !MODULE PVar_mod
 !C
+          !INTEGER  NNT
+          !PARAMETER (NNT=2000)    
+
+        !REAL*8 XC,YC,ZC,XTC,YTC,ZTC
+        !REAL*8 ARE,XF,YF,XK2,YK2,XCF
+        !REAL*8 VOLM,XB,YB,ZB
+
+        !REAL*8 AMAS(6,6),BDMP(6,6)  
+        !REAL*8 RMAS(6,6),CDMP(6,6),CRS(6,6),STKM(6,6),XIA(3,3)
+!C
+        !REAL*8 FORCEW(6),FORCE0(6),FORSCD(6),AMPJ(6)
+          !REAL*8 FORCE(6),FORCE_O(6)
+!C
+        !REAL*8 TRMAS(6,6),VISC(6,6)
+
+        !REAL*8  DISP(6),DSDT(6),DISP_O(6)
+        !real(8) ::  DSDT_O(6),DSDDTL(6),TRXYZ(3,3)   
+
+        !REAL*8  RESPR(6),RESPI(6)  
+!!
+!!
+        !END MODULE PVar_mod
+
+!!C
 !C =========================================
 !C  Varibles used in the Tri-pole transform 
 !C                                                         
--- src/property/bd_prop.f90
+++
@@ -1,7 +0,0 @@
-module body_property
-      implicit none
-      real(8) :: xc,yc,zc
-!      xc = 1
-!      yc = 2
-!      zc = 3
-end module
